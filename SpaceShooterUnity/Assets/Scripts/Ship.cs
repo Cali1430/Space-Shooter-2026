939aaa24a5fbf47e7ff788e48cc486d7d07ed827
WUStudent <wustudent@webster.edu>
2026-02-05 16:38:44 -0600

    added a new ship type, added sfx to all ships

--- SpaceShooterUnity/Assets/Scripts/Ship.cs
+++ SpaceShooterUnity/Assets/Scripts/Ship.cs
@@ -25,6 +25,12 @@ public class Ship : MonoBehaviour
 
     ParticleSystem thrustParticles;
 
+    public AudioSource pewPewAudioSource;
+
+    public bool canPewPew;
+
+    public AudioSource takeDamageAudioSource;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -57,11 +63,21 @@ public class Ship : MonoBehaviour
         GameObject newProjectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, transform.rotation);
         newProjectile.GetComponent<Rigidbody2D>().AddForce(transform.up * projectileVelocity);
         newProjectile.GetComponent<Projectile>().firingShip = gameObject;
+
+        float newPitch = Random.Range(0.9f, 1.1f);
+
+        pewPewAudioSource.pitch = newPitch; 
+
+        pewPewAudioSource.Play();
+
+        StartCoroutine(CoolDown());
+
         Destroy(newProjectile, 4);
     }
 
     public void TakeDamage(int damageToTake)
-    { 
+    {
+        takeDamageAudioSource.Play();
         currentHealth -= damageToTake;
 
         if (currentHealth <= 0)
@@ -75,5 +91,12 @@ public class Ship : MonoBehaviour
         //TODO: Make cool 'splosion particles
         GameObject newExplosion = Instantiate(explosionPrefab, projectileSpawnPoint.position,transform.rotation);
         Destroy(gameObject);
+    }
+
+    private IEnumerator CoolDown()
+    {
+        canPewPew = false;
+        yield return new WaitForSeconds(fireRate);
+        canPewPew = true;
     }
 }
